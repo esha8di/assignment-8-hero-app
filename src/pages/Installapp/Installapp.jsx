9a63fbcf3e42855b8db5ec887e7974fff8932b6a
Python <shuminnahar8di@gmail.com>
2025-11-10 00:35:10 +0600

    almost done

--- src/pages/Installapp/Installapp.jsx
+++ src/pages/Installapp/Installapp.jsx
@@ -1,65 +1,120 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData} from "react-router";
-import { getdata, remove} from "../Localstorage/Localstorage";
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
+import { getdata, remove } from "../Localstorage/Localstorage";
+
+import download from '../../assets/icon-downloads.png'
+
+import avgrating from "../../assets/icon-ratings.png";
+
+import review from "../../assets/icon-review.png";
+import { toast,ToastContainer } from "react-toastify";
+
 
 const Installapp = () => {
-    const datafromdetails = useLoaderData();
-    const [list,setList]=useState([])
-    
-    useEffect(()=>{
-        const getdatafromjs=getdata();
-    const installdata=datafromdetails.filter(data=>getdatafromjs.includes(String(data.id)))
-    // console.log(installdata);
-    setList(installdata)
+  const datafromdetails = useLoaderData();
+  const [list, setList] = useState([]);
 
-    console.log(list)
-
-    },[datafromdetails])
-        
-
-   
-    
-
-    const handleremove=(id)=>{
-        remove(id);
-        setList(prev => prev.filter(app => app.id !== id)); 
-    }
-     
-
-    return (
-        
-        <div className="my-[50px] flex flex-col justify-center items-center ">
-            
-                <div className="text-center mb-[60px] w-full  ">
-          <h2 className="text-xl font-bold">Trending Apps</h2>
-          <p className="text-gray-400">
-            Explore All Trending Apps on the Market developed by us
-          </p>
-        </div>
-           
-            <div className="down w-full flex justify-between items-center p-2 ">
-          <p className="text-xl font-bold">({list.length}) Apps Found</p>
-          
-          
-
-         
-         
-        </div>
-            <div>
-                {
-                list.map(data=><div>
-                    <button className='btn' onClick={()=>handleremove(data.id)}>
-                          { data.id}
-                    </button>
-                 
-
-                </div>)
-            }
-
-            </div>
-            
-        </div>
+  useEffect(() => {
+    const getdatafromjs = getdata();
+    const installdata = datafromdetails.filter((data) =>
+      getdatafromjs.includes(String(data.id))
     );
+    // console.log(installdata);
+    setList(installdata);
+
+    console.log(list);
+  }, [datafromdetails]);
+
+  const handleremove = (id) => {
+    remove(id);
+    setList((prev) => prev.filter((app) => app.id !== id));
+    toast("Uninstalled")
+  };
+
+  console.log(list)
+
+//    const {
+//   title,
+//     companyName,
+//     image,
+   
+//     downloads,
+//     reviews,
+//     ratingAvg,
+   
+//   } = list;
+
+  return (
+    <div className="my-[50px] w-[80%] mx-auto flex flex-col justify-center items-center ">
+      <div className="text-center mb-[60px] w-full  ">
+        <ToastContainer></ToastContainer>
+        <h2 className="text-xl font-bold">Trending Apps</h2>
+        <p className="text-gray-400">
+          Explore All Trending Apps on the Market developed by us
+        </p>
+      </div>
+
+      <div className="down w-full flex justify-between items-center p-2 ">
+        <p className="text-xl font-bold">({list.length}) Apps Found</p>
+        <p>hello</p>
+      </div>
+      <div className="w-full  rounded-2x ">
+        {list.map((data) => (
+          <div>
+            {/*  */}
+            <div className="appdetail  grid grid-cols-[10%_80%] gap-3 mb-3 bg-white rounded-2xl">
+
+        <div className="p-2 left">
+          <img className="rounded-2xl h-[50px] w-full" src={data.image} alt="not found" />
+        </div>
+
+        <div className="flex right  p-2 justify-between  ">
+          <div className="up">
+            <h2 className="text-xl font-bold">{data.title}</h2>
+            <div className="down flex justify-between gap-3 ">
+            <div className="flex justify-center items-center">
+              <span>
+                <img className="h-[15px]" src={download} alt="" />
+              </span>
+              
+              <p className=" ">{data.downloads}</p>
+            </div>
+            <div className="flex justify-center items-center g-1">
+              <span>
+                <img className="h-[15px]" src={avgrating} alt="" />
+              </span>
+              
+              <p className=" ">{data.ratingAvg}</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <span>
+                <img className="h-[20px]" src={review} alt="" />
+              </span>
+              
+              <p className="">{data.reviews}</p>
+            </div>
+          </div>
+            
+          </div>
+
+          
+
+          <div>
+              <button className="btn bg-purple-600 text-white" onClick={() => handleremove(data.id)}>
+                Uninstall
+              </button>
+            </div>
+          
+         
+        </div>
+      </div>
+            {/*  */}
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Installapp;
